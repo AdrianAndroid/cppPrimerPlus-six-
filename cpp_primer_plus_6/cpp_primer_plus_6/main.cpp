cee4442b1d286aa98243d0eee593453ab8114e06
zhaojian2 <zhaojian2@joyy.sg>
2021-12-12 11:31:23 +0800

    xcode

---
+++ cpp_primer_plus_6/cpp_primer_plus_6/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cpp_primer_plus_6
+//
+//  Created by 赵健 on 2021/12/11.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
