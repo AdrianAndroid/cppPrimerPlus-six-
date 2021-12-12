f2f83f8692c91d85c753ccca5f312175905d92e2
zhaojian2 <zhaojian2@joyy.sg>
2021-12-12 12:26:59 +0800

    RatePlayer

--- cpp_primer_plus_6/cpp_primer_plus_6/main.cpp
+++ cpp_primer_plus_6/cpp_primer_plus_6/main.cpp
@@ -7,24 +7,31 @@
 
 #include <iostream>
 #include "../Chapter_13/tabtenn0.hpp"
+#include "../Chapter_13/tabtenn1.hpp"
+
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
     using std::cout;
-    TableTennisPlayer player1("Chunk", "Blizzard", true);
-    TableTennisPlayer player2("Tara", "Boomdea", false);
-    player1.Name();
-    if(player1.HasTable()) {
+    using std::endl;
+    TableTennisPlayer player1("Tara", "Boomdea", false);
+    RatePlayer rplayer1(1140, "Mallory", "Duck", true);
+    rplayer1.Name();
+    if(rplayer1.HasTable()) {
         cout << ": has a table.\n";
     } else {
         cout << ": hasn't a table.\n";
     }
-    player2.Name();
-    if(player2.HasTable()) {
+    player1.Name();
+    if(player1.HasTable()){
         cout << ": has a table";
     } else {
-        cout << ": hasn't a table.\n";
+        cout << ": hasn't a table";
     }
-        return 0;
+    cout << "Name: ";
+    cout << "; Rating: " << rplayer1.Rating() << endl;
+    RatePlayer rplayer2(1212, player1);
+    cout << "Name: " ;
+    rplayer2.Name();
+    cout << "; Rating: " << rplayer2.Rating() << endl;
+    return 0;
 }
