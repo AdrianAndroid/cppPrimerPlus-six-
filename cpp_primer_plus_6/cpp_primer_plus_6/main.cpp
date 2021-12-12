8e2c13f769954c800b138c11ac2cff0a0daa3291
zhaojian2 <zhaojian2@joyy.com>
2021-12-12 17:27:55 +0800

    清空

--- cpp_primer_plus_6/cpp_primer_plus_6/main.cpp
+++ cpp_primer_plus_6/cpp_primer_plus_6/main.cpp
@@ -7,7 +7,6 @@
 
 #include <iostream>
 #include <string>
-#include "../Chapter_13/brass.hpp"
 
 const int CLIENTS = 4;
 using std::cin;
@@ -19,46 +18,6 @@ int main(int argc, const char * argv[]) {
     using std::cout;
     using std::endl;
     cout << "Hello" << endl;
-    
-    AcctABC * p_clients[CLIENTS];
-    std::string temp;
-    long tempnum;
-    double tempbal;
-    char kind;
-    
-    for(int i = 0; i < CLIENTS; i++) {
-        cout << "ENter client's nams: ";
-        getline(cin, temp);
-        cout << "Enter client's account number: ";
-        cin >> tempnum;
-        cout << "Enter opening balance: $";
-        cin >> tempbal;
-        cout << "Enter 1 for Brass account or " << " 2 for BrassPlus Account: ";
-        while(cin >> kind && (kind != '1' && kind != '2'))
-            cout << "Enter either 1 or 2: ";
-        if(kind == '1') {
-            p_clients[i] = new Brass(temp, tempnum, tempbal);
-        } else {
-            double tmax, trate;
-            cout << "Enter the overdraft rate " << "as a decimal fraction: ";
-            cin >> trate;
-            p_clients[i] = new BrassPlus(temp, tempnum, tempbal, tmax, trate);
-        }
-        while(cin.get() != '\n')
-              continue;
-
-    }
-    cout << endl;
-    for(int i = 0; i < CLIENTS; i++)
-    {
-        p_clients[i]->ViewAcct();
-        cout << endl;
-    }
-    for(int i = 0; i < CLIENTS; i++)
-    {
-        delete p_clients[i];
-    }
-    cout << "Done.\n";
     
     return 0;
 }
